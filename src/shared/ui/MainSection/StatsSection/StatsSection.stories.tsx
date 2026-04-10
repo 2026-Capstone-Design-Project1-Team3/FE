c2b1006e1655399df18a0e0c0a9f0cc307dd51da
yu-jio <83810621+yu-jio@users.noreply.github.com>
2026-04-10 23:18:53 +0900

    refactor:스토리북 내 파일위치 수정

--- src/shared/ui/MainSection/StatsSection/StatsSection.stories.tsx
+++ src/shared/ui/MainSection/StatsSection/StatsSection.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 import { StatsSection } from "./StatsSection";
 
 const meta: Meta<typeof StatsSection> = {
-  title: "Components/Sections/StatsSection",
+  title: "UI/MainSection/StatsSection",
   component: StatsSection,
   parameters: {
     layout: "fullscreen",
