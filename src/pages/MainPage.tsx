5ec8f7c1c4ed841629d67b50c0434ad3df2eea18
yu-jio <83810621+yu-jio@users.noreply.github.com>
2026-04-10 21:35:01 +0900

    feat:메인페이지 최종 조합(헤더제외)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,15 @@
+import { RecordSection } from "@/shared/ui/MainSection/RecordSection/RecordSection";
+import { StartSection } from "@/shared/ui/MainSection/StartSection/StartSection";
+import { StatsSection } from "@/shared/ui/MainSection/StatsSection/StatsSection";
+
 const MainPage = () => {
-  return <div>메인 페이지입니다.</div>;
+  return (
+    <div>
+      <StartSection />
+      <RecordSection />
+      <StatsSection />
+    </div>
+  );
 };
 
 export default MainPage;
