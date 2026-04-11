5e515155d201d986c8975fd1bfe0528033cadd11
yu-jio <83810621+yu-jio@users.noreply.github.com>
2026-04-11 16:25:20 +0900

    refactor:통계카드 메인페이지에서 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/shared/ui/MainSection/Header/Header";
 import { RecordSection } from "@/shared/ui/MainSection/RecordSection/RecordSection";
 import { StartSection } from "@/shared/ui/MainSection/StartSection/StartSection";
-import { StatsSection } from "@/shared/ui/MainSection/StatsSection/StatsSection";
 
 const MainPage = () => {
   return (
@@ -9,7 +8,6 @@ const MainPage = () => {
       <Header />
       <StartSection />
       <RecordSection />
-      <StatsSection />
     </div>
   );
 };
