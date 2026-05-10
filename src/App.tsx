41d4afd75426469325d4a32bdb8eb672f9dce7ce
yu-jio <83810621+yu-jio@users.noreply.github.com>
2026-05-10 13:42:53 +0900

    refactor: 메인페이지 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import GlobalToaster from "@/components/GlobalToaster";
+import { Layout } from "@/MainLayout";
 import LoginPage from "@/pages/auth/LoginPage";
 import SignupPage from "@/pages/auth/SignupPage";
+import CalibrationPage from "@/pages/CalibrationPage";
 import MainPage from "@/pages/MainPage";
+import MyPage from "@/pages/MyPage";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalToaster />
       <Routes>
-        <Route element={<MainPage />} path="/" />
         <Route element={<SignupPage />} path="/signup" />
         <Route element={<LoginPage />} path="/login" />
+        <Route element={<Layout />}>
+          <Route element={<MainPage />} path="/" />
+          <Route element={<MyPage />} path="/mypage" />
+          <Route element={<CalibrationPage />} path="/calibration" />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
