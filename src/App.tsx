a1cd44f6b6b391a70278c96e1d436510e07489fa
nyeong <hannayoung1210@gmail.com>
2026-05-17 21:13:40 +0900

    feat: InterviewRecordPage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import LoginPage from "@/pages/auth/LoginPage";
 import SignupPage from "@/pages/auth/SignupPage";
 import CalibrationPage from "@/pages/CalibrationPage";
 import InterviewPreparePage from "@/pages/InterviewPreparePage";
+import InterviewRecordPage from "@/pages/InterviewRecordPage";
 import MainPage from "@/pages/MainPage";
 import MyPage from "@/pages/MyPage";
 import PresentationPreparePage from "@/pages/PresentationPreparePage";
+import PresentationRecordPage from "@/pages/PresentationRecordPage";
 
 function App() {
   return (
@@ -20,9 +22,14 @@ function App() {
         <Route element={<MainLayout />}>
           <Route element={<MainPage />} path="/" />
           <Route element={<InterviewPreparePage />} path="/interview/prepare" />
+          <Route element={<InterviewRecordPage />} path="/interview/record" />
           <Route
             element={<PresentationPreparePage />}
             path="/presentation/prepare"
+          />
+          <Route
+            element={<PresentationRecordPage />}
+            path="/presentation/record"
           />
           <Route element={<MyPage />} path="/my" />
           <Route element={<CalibrationPage />} path="/my/calibration" />
