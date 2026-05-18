8f6009dbaffe6b64be8b099ff68266228aecfe1d
yu-jio <83810621+yu-jio@users.noreply.github.com>
2026-05-18 14:52:13 +0900

    feat: 목데이터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import { MainLayout } from "@/components/layouts/MainLayout";
 import LoginPage from "@/pages/auth/LoginPage";
 import SignupPage from "@/pages/auth/SignupPage";
 import CalibrationPage from "@/pages/CalibrationPage";
+import FolderDetailPage from "@/pages/FolderDetailPage";
 import InterviewPreparePage from "@/pages/InterviewPreparePage";
 import InterviewReportPage from "@/pages/InterviewReportPage";
+import InterviewResultPage from "@/pages/InterviewResultPage";
 import MainPage from "@/pages/MainPage";
 import MyPage from "@/pages/MyPage";
 import PresentationPreparePage from "@/pages/PresentationPreparePage";
 import PresentationReportPage from "@/pages/PresentationReportPage";
+import PresentationResultPage from "@/pages/PresentationResultPage";
+import ResultPage from "@/pages/ResultPage";
 
 function App() {
   return (
@@ -33,6 +37,20 @@ function App() {
           />
           <Route element={<MyPage />} path="/my" />
           <Route element={<CalibrationPage />} path="/my/calibration" />
+          <Route
+            element={<FolderDetailPage />}
+            path="/report/presentation/:folderId"
+          />
+          <Route
+            element={<FolderDetailPage />}
+            path="/report/interview/:folderId"
+          />
+          <Route element={<ResultPage />} path="/report" />
+          <Route
+            element={<PresentationResultPage />}
+            path="/report/presentation"
+          />
+          <Route element={<InterviewResultPage />} path="/report/interview" />
         </Route>
       </Routes>
     </BrowserRouter>
